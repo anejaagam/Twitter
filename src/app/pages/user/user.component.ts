97a25f8ceff546313b15537a7a381d3dfdcdf5bf
anejaagam <aneja.agamjot@gmail.com>
2022-07-26 19:14:27 -0700

    Updating Time

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -37,6 +37,7 @@ export class UserComponent implements OnInit {
   banner: Observable<string | null>;
   userTweets: any;
 
+
   constructor(public authservice : AuthService,
     public storage :AngularFireStorage,
     public afs : AngularFirestore,
